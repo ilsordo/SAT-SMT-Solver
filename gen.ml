ae1e01eac42c5ee873c0b9017b6a4e13f511ab56
Maxime Lesourd <maxime.lesourd@ens-lyon.fr>
2014-03-12 10:38:40 +0100

    générateur moins débile

--- gen.ml
+++ gen.ml
@@ -1,15 +1,15 @@
 open Sys
 open Printf
 
-let shuffle tab n = (* applique une permutation circulaire aléatoire sur le tableau tab de taille n *)
-  for i = 0 to n-1 do
+let shuffle tab l = (* applique une permutation circulaire aléatoire sur le tableau tab de taille n *)
+  let n = Array.length tab in
+  for i = 0 to l-1 do
       let x = Random.int (n - i) + i in
       let v = tab.(x) in
       tab.(x) <- tab.(i);
       tab.(i) <- v
   done
     
-
 let make_tab k = Array.init k (fun x -> x + 1) (* initialise un tableau de valeurs *)
 
 let print_tab l p tab = (* construit des littéraux à partir d'un tableau de valeurs, en choisissant aléatoirement les positivités *)
@@ -19,9 +19,9 @@ let print_tab l p tab = (* construit des littéraux à partir d'un tableau de va
   done
 
 let gen_formule n l k p = (* génère une formule aléatoire de n variables et k clauses de longueur l chacune *)
+  let tab = (make_tab n) in
   for i = 1 to k do
-    let tab = (make_tab n) in
-    shuffle tab n;
+    shuffle tab l;
     fprintf p "%a0\n" (print_tab l) tab
   done
 
@@ -37,13 +37,3 @@ let _ =
         eprintf "Error : la taille des clauses est supérieure au nombre de variables (l>n)\n%!"
     else raise (Failure "")
   with Failure _ -> eprintf "Usage : gen n l k\n%!"
-
-
-
-
-
-
-
-
-
-
