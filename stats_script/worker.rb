ae0e95ae72d0f21dfa09c7a6441c0248b024d6b1
Maxime Lesourd <maxime.lesourd@ens-lyon.fr>
2014-03-22 23:40:04 +0100

    zefo

---
+++ stats_script/worker.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/ruby
+
+module Tests
+  Heuristics = ["rand_rand","rand_mf","next_rand","next_mf","moms","dlis"]
+  Algos = ["wl","dpll"]
+end
+
+class MyRange
+  attr:
+  def initialize(first,last,step = 1)
+    
+
+if __FILE__ == $0
+  puts "Hello World"
+end
