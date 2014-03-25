f14adbe7c1ad0ffb1034255cb8faf2e3c872a7e8
Maxime Lesourd <maxime.lesourd@ens-lyon.fr>
2014-03-25 20:59:01 +0100

    generateurs

--- src/gen/gcolor.ml
+++ src/gen/gcolor.ml
@@ -1,4 +1,3 @@
-open Config_random
 open Printf
 
 let random_edges n p pp = (* génère un graphe aléatoire de n sommets, avec proba p pour chaque arête *)
@@ -10,10 +9,12 @@ let random_edges n p pp = (* génère un graphe aléatoire de n sommets, avec pr
  done
 
 
-let gen () =
-  let (n,p) = (config.param1,config.param4) in
-    if (0.<=p && p<=1.) then
-      printf "p edge %d %d\n%t%!" n 0 (random_edges n p) (*** il faut trouver un moyen de connaitre le nb d'aretes créées (pour l'instant j'ai mis 0) *)
-    else
-      eprintf "Error : p doit être une probabilité (0<=p<=1)\n%!"
+let gen n p =
+  if (0.<=p && p<=1.) then
+    printf "p edge %d %d\n%t%!" n 0 (random_edges n p) (*** il faut trouver un moyen de connaitre le nb d'aretes créées (pour l'instant j'ai mis 0) *)
+  else
+    begin
+      eprintf "Error : p doit être une probabilité (0<=p<=1)\n%!";
+      exit 1
+    end
 (*** suite de la remarque ci-dessus : en fait, connaitre le nb d'arêtes du graphe n'est pas nécessaire dans notre algo. Donc on laisse 0 ? *)
--- src/gen/gen.ml
+++ src/gen/gen.ml
@@ -1,14 +1,17 @@
-open Config_random
+let parse_args () =
+  let args = Sys.argv in
+  let parse_int i = int_of_string args.(i) in
+  if Array.length args > 1 then
+    match (args.(1), Array.length args) with
+      | "-color", 4 -> Gcolor.gen (parse_int 2) (float_of_string args.(3))
+      | "-tseitin", 4 -> Gtseitin.gen (parse_int 2) (parse_int 3)
+      | "-cnf", 5 -> Gcnf.gen (parse_int 2) (parse_int 3) (parse_int 4)
+      | _, 5 -> Gcnf.gen (parse_int 1) (parse_int 2) (parse_int 3)
+      | _ -> Printf.eprintf "Usage : gen -[cnf|color|tseitin] parametres\n%!"; exit 1
 
 let main () =
-  parse_args();
   Random.self_init();
-  match config.random_type with
-    | Cnf -> Gcnf.gen ()
-    | Tseitin -> Gtseitin.gen ()
-    | Color -> Gcolor.gen ()
-  ;
+  parse_args ();
   exit 0
 
 let _ = main()
-
