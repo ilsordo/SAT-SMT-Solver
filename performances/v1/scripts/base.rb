e8b0c60e1af20a511d587236a018b37d353dc1bb
yhamoudi <yassine.hamoudi@ens-lyon.fr>
2014-04-08 23:32:27 +0200

    r

--- performances/v1/scripts/base.rb
+++ performances/v1/scripts/base.rb
@@ -215,12 +215,12 @@ class Problem
   
   def gen
     temp = Tempfile.open("sat")
-    system "./gen -tseitin #{@n} #{@l} > #{temp.path}" ######### !!!!!!!!!!!!!!!!
+    system "./gen #{@n} #{@l} #{@k} > #{temp.path}"
     Proc::new { |timeout|
       timeout ||= 0
       result = Result::new
       #puts "./main -algo #{@algo} -h #{@heuristic} #{temp.path} 2>&1"
-      IO::popen "if ! timeout #{timeout} ./main -tseitin -algo #{@algo} -h #{@heuristic} #{temp.path} 2>&1; then echo \"Timeout\"; fi" do |io|  ######### !!!!!!!!!!!!!!!
+      IO::popen "if ! timeout #{timeout} ./main -algo #{@algo} -h #{@heuristic} #{temp.path} 2>&1; then echo \"Timeout\"; fi" do |io|
         io.each do |line|
           case line
           when /\[stats\] (?<stat>.+) = (?<value>\d+)/
