7c6e656b8bf7aad38147322e7d52ce565bb24a15
Maxime Lesourd <maxime.lesourd@ens-lyon.fr>
2014-04-08 21:29:31 +0200

    scripts

---
+++ performances/scripts/skel.p
@@ -0,0 +1,21 @@
+reset
+
+### pour pdf
+# set term pdfcairo
+# set output "courbe1.pdf" # le nom du fichier qui est engendre
+
+
+set title "#{title}"
+set xlabel "#{xlabel}"
+set ylabel "#{ylabel}"
+# set autoscale x
+
+set style data linespoints
+
+set pointsize 1
+
+set datafile missing "0"
+
+plot for [i=2:#{ncols}] "#{data}" using (column(i)):xticlabels(1) title columnheader(i)
+
+exit
---
+++ performances/scripts/tseitin.rb
@@ -0,0 +1,53 @@
+#!/usr/bin/ruby
+# -*- coding: utf-8 -*-
+
+def tseitin(name,threads)
+  db = Database::new
+
+  algos = ["dpll","wl"]
+  heuristics = ["dlcs","dlis","jewa"]
+  l = [100,200,300,400,500,1000,1500,2000,2500,3000,3500,4000]
+     
+  def boucle(l,algos,heuristics,&block)
+    l.each do |c|
+      2.times do
+        p = ProblemTseitin::new(100,c)
+        puts p
+        proc = p.gen
+        algos.each do |algo|
+          heuristics.each do |h|
+            report = Report::new
+            begin
+              entry,result = proc.call(algo,h,150)
+              report << result
+              yield(entry,report) if result
+            end
+          end
+        end
+      end
+    end
+  end
+  
+  threads.times do 
+    Thread::new do
+      boucle(l,algos,heuristics) { |entry,report| db.record(entry,report) }
+    end
+  end
+
+  while Thread::list.length != 1 do
+    system "date -R"
+    puts "Saving"
+    db.save name
+    puts "Done"
+    sleep 60
+  end
+
+  (Thread::list - [Thread::current]).each do |t|
+    t.join
+  end
+
+  puts "Saving"
+  db.save name
+  puts "Done"
+end
+
