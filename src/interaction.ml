febe63c8147f5104f0fc75054e4a32b9d3e169c7
Maxime Lesourd <maxime.lesourd@ens-lyon.fr>
2014-04-24 23:38:11 +0200

    resol broken

--- src/interaction.ml
+++ src/interaction.ml
@@ -11,7 +11,8 @@ let print_values p (formule:formule) =
   fprintf p "Current values (level) :\n";
   formule#get_paris#iter (fun v b -> fprintf p "%d -> %B (%d)\n" v b (formule#get_level v)) 
 
-let print_graph (formule:formule) (pari,assignations) level clause p =
+(** Graphe *)
+let print_graph (formule:formule) (pari,assignations) level (clause:clause) p =
   (* Indique si une variable (le literal associé) a été vu et si sa cause a été élucidée ainsi que son nom*)
   let seen = new vartable 0 in
 
@@ -35,7 +36,6 @@ let print_graph (formule:formule) (pari,assignations) level clause p =
           fprintf p "subgraph cluster_0{%s}\n%s -> %s\n" tag tag conseq in
 
   let explore_clause conseq uip_found needed clause =
-    debug#p 2 "Entrée par %s dans la clause : \n%a\n Needed : %B" conseq clause#print () needed;
     clause#get_vpos#iter_all (process conseq uip_found needed true);
     clause#get_vneg#iter_all (process conseq uip_found needed false) in
 
@@ -47,8 +47,7 @@ let print_graph (formule:formule) (pari,assignations) level clause p =
           seen#set v (true,str_of_lit (not b, v)); in
 
   let is_uip () =
-    debug#p 2 "Analyse de l'uip:\n";
-    match seen#fold (fun v (known, tag) (res, count) -> debug#p 2 "%d : %B # %d" v known count;if known then (res, count) else (Some tag, count + 1)) (None, 0) with
+    match seen#fold (fun v (known, tag) (res, count) -> if known then (res, count) else (Some tag, count + 1)) (None, 0) with
       | (Some tag, 1) -> Some tag
       | _ -> None
   in
@@ -82,16 +81,127 @@ let print_graph (formule:formule) (pari,assignations) level clause p =
                 follow_lit lit false;*)
                 uip_found in
         explore found q in
-  debug#p 2 "Clause conflit : %a\nPari : %s\n%a" clause#print () (str_of_lit pari) print_values formule;
   fprintf p "digraph G{\nrankdir = LR;\nnode[style=filled,shape=circle,width=1];\nsubgraph cluster_0{style=dashed;label=\"Clause\"};\nconflict [label=\"Conflict!\",shape=ellipse,fillcolor=crimson];\n";
   explore_clause "conflict" false true clause;
   explore false assignations;
   fprintf p "%s[fillcolor=chartreuse]\n}\n%!" (str_of_lit pari)
-   
 
-let print_resolution (formule:formule) (pari,assignations) level clause =
-  ()
- 
+
+(** Resolution *)
+type split_clause = (literal list*literal list*literal) 
+
+type proof = Tag of int | Base of split_clause*int | Resol of proof*split_clause*split_clause*int
+
+let print_resolution (formule:formule) (pari,assignations) level clause p =
+  
+  let print_lit p (b,v) =
+    if b then
+      fprintf p "\\varv{%d}" v
+    else
+      fprintf p "\\varf{%d}" v in
+
+  (*let print_raw_clause p = function
+    | [] -> assert false (* Que fait une clause vide ici? *)
+    | t::q ->
+    print_lit p t;
+    List.iter (fun lit -> fprintf p "\\lor %a" print_lit lit) q in*)
+
+  let print_split_clause p (lower,curr,join) =
+    let started =
+      match lower with
+        | [] -> false
+        | t::q -> 
+            print_lit p t;
+            List.iter (fun lit -> fprintf p "\\lor %a" print_lit lit) q;
+            true in
+    let started =
+      match curr with
+        | l when started ->
+            List.iter (fun lit -> fprintf p "\\lor {\\color{blue} %a}" print_lit lit) l;
+            true
+        | [] -> false
+        | t::q -> 
+            fprintf p "{\\color{blue} %a}" print_lit t;
+            List.iter (fun lit -> fprintf p "\\lor {\\color{blue} %a}" print_lit lit) q;
+            true in
+    if started then
+      fprintf p "\\lor ";
+    fprintf p "{\\color{red} %a}\n%!" print_lit join in
+  
+  let print_proof proof_id proof =
+    let rec aux p = function
+      | Tag i ->
+          fprintf p "\\preuve{%d}\n" i
+      | Base (c,id) ->
+          fprintf p "\\cl{%d} %a\n" id print_split_clause c
+      | Resol (proof,conclusion,clause,id) ->
+          fprintf p "\\inferrule{\n%a\\and\n\\cl{%d} %a}{\n%a}\n" 
+            aux proof 
+            id 
+            print_split_clause clause
+            print_split_clause conclusion in
+    fprintf p "\\begin{mathpar}\\preuve{%d}:~%a\\end{mathpar}\n" proof_id aux proof in
+  
+  let split_clause ((b_join,v_join) as join) (clause:clause) =
+    let aux b v (lower,curr) =
+      if (b,v) = join then
+        (lower,curr)
+      else
+        if formule#get_level v = level then
+          (lower,(b,v)::curr)
+        else
+          ((b,v)::lower,curr) in
+    assert(v_join = 0 || clause#mem_all b_join v_join); (** Horreur nécessaire *)
+    let (lower,curr) = clause#get_vpos#fold_all (aux true) (clause#get_vneg#fold_all (aux false) ([],[])) in
+    (List.sort compare lower,List.sort compare curr) in
+
+  let resol (lower,curr,(b,v)) (clause:clause) =
+    let simplify x = function (* Pour un fold_right uniquement *)
+      | t::q when x = t -> q
+      | l -> x::l in
+    let (lower',curr') = split_clause (not b,v) clause in
+    let lower = List.(fold_right simplify (merge compare lower lower') []) in
+    let curr = List.(fold_right simplify (merge compare curr curr') []) in
+    (lower,curr) in
+
+  let rec find_next curr = function
+    | [] -> assert false
+    | (b,v)::q when List.mem (b, v) curr -> ((b,v),q)
+    | _::q -> find_next curr q in
+  
+
+  let rec build_proof proof (lower,curr,(b,v)) remaining =
+    match curr with
+      | [] -> print_proof 1 proof
+      | _ ->
+          let clause =
+            match formule#get_origin v with
+              | None -> assert false
+              | Some clause -> clause in 
+          let (lower',curr') = resol (lower,curr,(not b, v)) clause in
+          let (join,remaining) = find_next curr' remaining in
+          let conclusion = (lower',List.filter ((<>) join ) curr',join) in (* Join the Empire? *)
+          build_proof (Resol(proof,conclusion,(lower',curr',(not b, v)),clause#get_id)) conclusion remaining in
+  debug#p 0 "%d" level;
+  fprintf p "
+\\documentclass{article}
+\\usepackage{mathpartir}
+\\usepackage[utf8]{inputenc}
+\\usepackage{color}
+\\newcommand{\\non}[1]{\\overline{#1}}
+\\newcommand{\\varv}[1]{x_{#1}}
+\\newcommand{\\varf}[1]{\\non{\\varv{#1}}}
+\\newcommand{\\cl}[1]{\\mathtt{C_{#1}:~}}
+\\newcommand{\\preuve}[1]{\\mathtt{\\Pi_{#1}}}
+\\begin{document}
+Preuve de résolution :";
+  let (lower,curr) = split_clause (false,0) clause in
+  let (join,remaining) = find_next curr assignations in
+  let init = (lower,List.filter ((<>) join ) curr,join) in
+  build_proof (Base(init,clause#get_id)) init remaining;
+  fprintf p "\\end{document}\n%!"
+  
+(** Boucle *)
 
 type interaction =
   | U of (unit -> bool)
@@ -122,20 +232,34 @@ object
       | (name,_,doc)::q -> 
           fprintf p "%c\t%s\n" name doc;
           print_handlers q in
-    let print_graph = match etat.tranches with
-      | [] -> assert false
-      | tranche::_ -> 
-          fun file ->
-            try
-              let out = open_out (file^".dot") in
-              print_graph formule tranche etat.level clause out;
-              true
-            with 
-                Sys_error s -> 
-                  fprintf p "Error : %s\n" s;
-                  true in
-    let print_resolution _ =
-      true in
+    let print_graph =
+      match etat.tranches with
+        | [] -> assert false
+        | tranche::_ -> 
+            fun file ->
+              try
+                let out = open_out (file^".dot") in
+                print_graph formule tranche etat.level clause out;
+                fprintf p "Output written to %s, view it with ./print_graph %s\n" (file^".dot") file;
+                true
+              with 
+                  Sys_error s -> 
+                    fprintf p "Error : %s\n" s;
+                    true in
+    let print_resolution =
+      match etat.tranches with
+        | [] -> assert false
+        | tranche::_ -> 
+            fun file ->
+              try
+                let out = open_out (file^".tex") in
+                print_resolution formule tranche etat.level clause out;
+                fprintf p "Output written to %s, view it with ./print_resol %s\n" (file^".tex") file;
+                true
+              with 
+                  Sys_error s -> 
+                    fprintf p "Error : %s\n" s;
+                    true in
     let print_values () =
       print_values p formule;
       true in
