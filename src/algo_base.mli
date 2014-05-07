2de87fd6055e89e67104741a3576b73b9d53ce84
yhamoudi <yassine.hamoudi@ens-lyon.fr>
2014-05-07 17:35:17 +0200

    modif de first

--- src/algo_base.mli
+++ src/algo_base.mli
@@ -1,7 +1,7 @@
 open Clause
 open Formule
 
-type tranche = literal * literal list 
+type tranche = bool*literal * literal list 
 
 type etat = {
   tranches : tranche list;
@@ -12,7 +12,7 @@ exception Conflit_prop of (clause*(literal list)) (* permet de construire une tr
 
 module type Algo_base =
 sig
-  type formule = private #formule (* J'ai trouvé! *)
+  type formule = private #formule
 
   val name : string
 
