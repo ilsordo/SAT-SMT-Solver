278075a66f434f3e1bd43bd741c56342c633e913
yhamoudi <yassine.hamoudi@ens-lyon.fr>
2014-05-14 15:10:11 +0200

    algo smt

--- src/smt/smt.ml
+++ src/smt/smt.ml
@@ -15,15 +15,35 @@ struct
             begin
               try
                 let etat_smt = Smt.propagate acc etat_smt in
-              (* On a fini *)
+                  Solvable .......   (* On a fini *)
               with
                 | Conflit_smt clause ->
                     let (undo_list,next_bet) = Algo.backtrack clause in
                     let etat_smt = Smt.backtrack undo_list etat_smt in
                     aux etat_smt next_bet period 0 []
             end
-        | Bet_done (assignations,next_bet,backtrack) (******************)
-
+        | Bet_done (assignations,next_bet,backtrack) -> 
+        (******************)
+            let acc = (List.rev_append assignations acc) in (* pas sur pour le rev, pas besoin du append ? *)
+            if date = period then (* c'est le moment de propager dans la théorie *)
+              begin
+                try
+                  let etat_smt = Smt.propagate acc etat_smt in 
+                    aux etat_smt next_bet period (date+1) []
+                with
+                  | Conflit_smt clause ->
+                      let (undo_list,next_bet) = Algo.backtrack clause in
+                      let etat_smt = Smt.backtrack undo_list etat_smt in
+                      aux etat_smt next_bet period 0 []      
+              end        
+            else
+              aux etat_smt next_bet period (date+1) acc
+        | Conflit_dpll (undo_list,next_bet) -> (* renommer ce conflit ? *) (* je suppose ici que dpll a déjà backtracké dans son coin *)
+            let etat_smt = Smt.backtrack undo_list etat_smt in
+            aux etat_smt next_bet period 0 []  
+        | Unsolvable -> Unsolvable          
+        (******************)
+     
     let (etat_smt,n,cnf) = Smt.init data in
     match algo n cnf with
       | Backtrack -> Unsolvable
