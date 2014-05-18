8a40ec0020e4a3d892f5db2aa78bb3b5d296a908
yhamoudi <yassine.hamoudi@ens-lyon.fr>
2014-05-18 16:15:51 +0200

    mouvement de masse

--- src/theories/congruence_closure.ml
+++ src/theories/congruence_closure.ml
@@ -1,8 +1,9 @@
-
+open Formula_tree
+open Union_find
 
 type term = Var of string | Fun of term*term
 
-type atom = Eq of term*term | Ined of term*term
+type atom = Eq of term*term | Ineq of term*term
 
 
 let parse_atom s =
