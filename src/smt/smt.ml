b9be0c40204a9fc92cb4c5655e1613b564d05f6c
yhamoudi <yassine.hamoudi@ens-lyon.fr>
2014-05-21 22:46:41 +0200

    e

--- src/smt/smt.ml
+++ src/smt/smt.ml
@@ -6,6 +6,7 @@ open Algo_base
 open Reduction
 open Answer
 open Formule
+open Debug
 
 module Make_smt = functor(Dpll : Algo_parametric) -> functor (Smt : Smt_base) ->
 struct
@@ -26,15 +27,20 @@ struct
     (* Faire un pari, propager, se relever en cas de conflit *)
     let rec aux reduction etat_smt next_bet period date acc =
       match next_bet () with
-        | No_bet (values,backtrack) ->
+        | No_bet (values,backtrack) -> (* on regarde si la théorie est consistante *)
             begin
               try
+                stats#start_timer "SMT propagate (s)";
                 let etat_smt = Smt.propagate reduction (List.rev acc) etat_smt in
+                stats#stop_timer "SMT propagate (s)";
                 Solvable (values, Smt.print_answer reduction etat_smt values)
               with
                 | Smt.Conflit_smt (clause,etat_smt) ->
+                    stats#stop_timer "SMT propagate (s)";
                     let (undo_list,next_bet) = backtrack clause in
+                    stats#start_timer "SMT backtrack (s)";
                     let etat_smt = Smt.backtrack reduction undo_list etat_smt in
+                    stats#stop_timer "SMT backtrack (s)";
                     aux reduction etat_smt next_bet period 0 []
             end
         | Bet_done (assignations,next_bet,backtrack) -> 
@@ -42,18 +48,25 @@ struct
             if date = period then (* c'est le moment de propager dans la théorie *)
               begin
                 try
+                  stats#start_timer "SMT propagate (s)";
                   let etat_smt = Smt.propagate reduction (List.rev acc) etat_smt in 
+                  stats#stop_timer "SMT propagate (s)";
                   aux reduction etat_smt next_bet period (date+1) []
                 with
                   | Smt.Conflit_smt (clause,etat_smt) ->
+                      stats#stop_timer "SMT propagate (s)";
                       let (undo_list,next_bet) = backtrack clause in
+                      stats#start_timer "SMT backtrack (s)";
                       let etat_smt = Smt.backtrack reduction undo_list etat_smt in
+                      stats#stop_timer "SMT backtrack (s)";
                       aux reduction etat_smt next_bet period 0 []      
               end        
             else
               aux reduction etat_smt next_bet period (date+1) acc
         | Conflit_dpll (undo_list,next_bet) -> (* on suppose ici que dpll a déjà backtracké dans son coin *)
+            stats#start_timer "SMT backtrack (s)";
             let etat_smt = Smt.backtrack reduction undo_list etat_smt in
+            stats#stop_timer "SMT backtrack (s)";
             aux reduction etat_smt next_bet period 0 []  
     in
     
@@ -61,7 +74,9 @@ struct
     let etat_smt = Smt.init reduction in (* initialisation de l'etat du smt *)
     try 
       let (prop_init, next_bet) = Dpll.run heuristic cl interaction Smt.pure_prop reduction#max cnf in
+      stats#start_timer "SMT propagate (s)";
       let etat_smt = Smt.propagate reduction prop_init etat_smt in
+      stats#stop_timer "SMT propagate (s)";
       aux reduction etat_smt next_bet period 1 []
     with
       | Smt.Conflit_smt _ 
